47a9a90c3c37a7fc7199134c3b6817e818c0bd80
Devilreina <devilreinatv@gmail.com>
2024-06-28 13:14:27 +0300

    commit

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,20 +1,37 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val searchButton = findViewById<LinearLayout>(R.id.search_button)
+        val mediaLibraryButton = findViewById<LinearLayout>(R.id.media_library_button)
+        val settingsButton = findViewById<LinearLayout>(R.id.settings_button)
+
+        // Анонимный класс для первой кнопки
+        val searchClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Поиск", Toast.LENGTH_SHORT).show()
+            }
+        }
+        searchButton.setOnClickListener(searchClickListener)
+
+        // Лямбда-выражение для второй кнопки
+        mediaLibraryButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Медиатека", Toast.LENGTH_SHORT).show()
+        }
+
+        // onClick для третьей кнопки через xml атрибут
+        settingsButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Настройки", Toast.LENGTH_SHORT).show()
         }
     }
 }
